25ccc02b5620a8822ee41cb6c257b74de9fc9e63
depeenderrathore <deepenrathore28@gmail.com>
2015-10-24 02:43:51 +0530

    Blade intro

--- laralearn/app/Http/routes.php
+++ laralearn/app/Http/routes.php
@@ -12,27 +12,5 @@
 */
 
 Route::get('/', function () {
-    return 'You have arried';
-});
-
-
-Route::get('/milk/{flavor?}',function($flavor = null){
-	if($flavor == null){
-		return "All the flavours are nice";
-	}
-	return $flavor .' milk is nice';
-});
-
-Route::get('/articles/{title?}',function($title = null){
-	if($title == null){
-		return 'This is the list of all articles';
-	}
-	return 'We found article named ' . $title;
-});
-
-Route::get('/a/{test}/b/{title?}',function($test,$title = null){
-	if($title == null){
-		return 'This is the list of all articles';
-	}
-	return 'test = ' . $test . ' title = ' . $title;
+    return view('welcome');
 });
