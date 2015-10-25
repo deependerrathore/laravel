896f8f351adbc247f6fc75c00b4b58f5705e7e22
depeenderrathore <deepenrathore28@gmail.com>
2015-10-25 17:03:07 +0530

    Magic method in SQL query

--- laralearn/app/Http/routes.php
+++ laralearn/app/Http/routes.php
@@ -13,6 +13,20 @@
 
 
 Route::get('/', function () {
+
+	/*
+		Small note:we can use magic methods in our where clauses
+			$users = DB::table('users')->where('occupation','Engineer')->first();
+			$users = DB::table('users')->whereOccupation('Engineer')->first();
+
+	*/
+	//Select * from users where id=3
+	$users = DB::table('users')->where('id',3)->first();//this will first execute get() then return first element
+	dd($users);
+
+	//Select * from users where id=3
+	$users = DB::table('users')->where('id',3)->get();
+	dd($users);
 	//Select * from users;
 	$users = DB::table('users')->get();
 	dd($users);
